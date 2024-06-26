c1b36c0b90fd29e7d532e66576a4e1e463492f1e
LeonardoSantos16 <87033299+LeonardoSantos16@users.noreply.github.com>
2024-06-26 10:42:00 -0300

    first-commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function clickHandler(e){
+    e.preventDefault()
+
+    document.querySelector('.screen1').classList.toggle('hide')
+    document.querySelector('.screen2').classList.toggle('hide')
+
+}
